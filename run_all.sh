92590b58dcf1a9ce6ab35999fe9e3518072c203c
Roman Joeres <joeres.roman@web.de>
2024-08-23 23:12:31 +0200

    setup for recalculation of sensitivity scores

---
+++ run_all.sh
@@ -0,0 +1,5 @@
+python -m gifflar.train configs/downstream/all.yaml > log_all.txt
+python -m gifflar.train configs/downstream/both.yaml > log_both.txt
+python -m gifflar.train configs/downstream/lppe.yaml > log_lppe.txt
+python -m gifflar.train configs/downstream/rwpe.yaml > log_rwpe.txt
+python -m gifflar.train configs/downstream/sens.yaml > log_sens.txt
